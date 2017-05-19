12b70c883fa57d2a26c71fa930bc346c47186f59
刘鹏飞 <297770327@qq.com>
2017-05-19 17:20:13 +0800

    完善下发点击效果

--- rn.js/component/gateway.js
+++ rn.js/component/gateway.js
@@ -4,7 +4,7 @@
 import React from 'react';
 import {
     View, Text, Image, TextInput, Modal, Platform,
-    Slider, Switch, NavigatorIOS, StyleSheet, TouchableHighlight, StatusBar, TouchableOpacity, ListView
+    Slider, Switch, NavigatorIOS, StyleSheet, TouchableHighlight, StatusBar, TouchableOpacity, ListView,AsyncStorage
 } from 'react-native';
 var AlertIOS = Platform.select({
     ios: () => require('AlertIOS'),
@@ -17,7 +17,7 @@ import Orientation from 'react-native-orientation';
 import WebViewBridge from 'react-native-webview-bridge';
 import Test from './test.ios';
 
-var data;
+var data = [];
 export default class Gateway extends React.Component {
     constructor(props) {
         super(props);
@@ -27,38 +27,76 @@ export default class Gateway extends React.Component {
             dataSource: ds.cloneWithRows([]),
         };
         var _this = this;
-        if (props.batch){
-            fetch("http://121.42.253.149:18816/v1_0_0/homesSataionData?station_id=" + props.station_id + "&access_token=591d486f624d470005616b3f&tag_id=" + props.tag_id + "")
-                .then((response) => response.json())
-                .then((responseJson) => {
-                    data = responseJson.issued;
-                    for (var i = 0; i < data.length; i++) {
-                        data[i].check = 1;
-                    }
-                    _this.setState({
-                        dataSource: ds.cloneWithRows(data),
-                        sliderValue: responseJson.issued[0].data_value / 100,
-                    });
+        AsyncStorage.getItem("access_token", function (errs, result) {
+            if (!errs) {
+                fetch("http://121.42.253.149:18816/v1_0_0/homesSataionData?station_id=" + props.station_id + "&access_token=" + result + "&tag_id=" + props.tag_id + "")
+                    .then((response) => response.json())
+                    .then((responseJson) => {
+                        data=[];
+                        if (props.batch) {
+                            data = responseJson.issued;
+                            for (var i = 0; i < data.length; i++) {
+                                data[i].check = 1;
+                            }
+                        }
+                        _this.setState({
+                            dataSource: ds.cloneWithRows(data),
+                            sliderValue: responseJson.data_value / 100,
+                        });
 
-                })
-                .catch((error) => {
-                    console.error(error);
-                    AlertIOS.alert(
-                        '提示',
-                        '网络连接失败',
-                    );
-                });
-        }
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                        AlertIOS.alert(
+                            '提示',
+                            '网络连接失败',
+                        );
+                    });
+            }
+        });
+
+
 
     }
 
 
 
     confirm() {
-        AlertIOS.alert(
-            '提示',
-            '下发成功',
-        );
+        var _this = this;
+        var station_id = '';
+        _this.props.close();
+        if (data.length) {
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].check) {
+                    station_id += data[i].station_id + ",";
+                }
+            }
+        } else {
+            station_id = _this.props.station_id;
+        }
+        AsyncStorage.getItem("access_token", function (errs, result) {
+            if (!errs) {
+                var url="http://121.42.253.149:18816/v1_0_0/gateway?station_id=" + station_id + "&access_token=" + result + "&tag_id=" + _this.props.tag_id + "&data_value=" + _this.state.sliderValue.toFixed(2) * 100;
+                console.log(url);
+               
+                fetch(url,{method: 'POST'})
+                    .then((response) => response.json())
+                    .then((responseJson) => {
+                        AlertIOS.alert(
+                            '提示',
+                            '下发成功',
+                        );
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                        AlertIOS.alert(
+                            '提示',
+                            '网络连接失败',
+                        );
+                    });
+            }
+        });
+
     }
 
     check(rowID) {
@@ -73,46 +111,46 @@ export default class Gateway extends React.Component {
     render() {
         return (
             <View style={styles.all}>
-                        <View style={styles.sliderView}>
-                            <Text style={{ fontSize: 30, color: '#E0960A', marginTop: 20, }}>{parseInt((this.state.sliderValue) * 100)}</Text>
+                <View style={styles.sliderView}>
+                    <Text style={{ fontSize: 30, color: '#E0960A', marginTop: 20, }}>{parseInt((this.state.sliderValue) * 100)}</Text>
+                </View>
+                <View style={styles.sliderView}>
+                    <Text style={{}}> 0 </Text>
+                    <Slider style={{ flex: 0.8 }}
+                        value={this.state.sliderValue}
+                        onSlidingComplete={() => console.log('当前的值为' + this.state.sliderValue)}
+                        onValueChange={(sliderValue) => this.setState({ sliderValue: sliderValue })} />
+                    <Text style={{}}> 100 </Text>
+                </View>
+                <View style={styles.confirmView}>
+                    <TouchableOpacity activeOpacity={0.5} onPress={this.confirm.bind(this)}>
+                        <View style={{ backgroundColor: '#E0960A', height: 30, width: 100, borderRadius: 50, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', }}>
+                            <Text style={{ fontSize: 14, color: '#ffffff' }}>下发</Text>
                         </View>
-                        <View style={styles.sliderView}>
-                            <Text style={{}}> 0 </Text>
-                            <Slider style={{ flex: 0.8 }}
-                                value={this.state.sliderValue}
-                                onSlidingComplete={() => console.log('当前的值为' + this.state.sliderValue)}
-                                onValueChange={(sliderValue) => this.setState({ sliderValue: sliderValue })} />
-                            <Text style={{}}> 100 </Text>
-                        </View>
-                        <View style={styles.confirmView}>
-                            <TouchableOpacity activeOpacity={0.5} onPress={this.confirm.bind(this)}>
-                                <View style={{ backgroundColor: '#E0960A', height: 30, width: 100, borderRadius: 50, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', }}>
-                                    <Text style={{ fontSize: 14, color: '#ffffff' }}>下发</Text>
+                    </TouchableOpacity>
+                    <Text style={{ marginTop: 10, color: '#939495' }}>{this.state.confirmTime}</Text>
+                </View>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    contentContainerStyle={styles.listView}
+                    enableEmptySections={true}
+                    renderRow={(rowData, sectionID, rowID) => {
+                        return (
+                            <TouchableOpacity onPress={this.check.bind(this, rowID)}>
+                                <View style={styles.listItem}>
+                                    <Text style={styles.listItemName}>{rowData.station_name}</Text>
+                                    <View style={styles.content}>
+                                        <View style={styles.textAll}>
+                                            <Text style={styles.listItemValue}>{rowData.data_value}</Text>
+                                            <Image source={rowData.check ? require('../icons/check_pre.png') : require('../icons/check_nor.png')} style={styles.checkImage}></Image>
+                                        </View>
+                                        <Text style={styles.time}>{rowData.data_time}</Text>
+                                    </View>
                                 </View>
                             </TouchableOpacity>
-                            <Text style={{ marginTop: 10, color: '#939495' }}>{this.state.confirmTime}</Text>
-                        </View>
-                        <ListView
-                            dataSource={this.state.dataSource}
-                            contentContainerStyle={styles.listView}
-                            enableEmptySections={true}
-                            renderRow={(rowData, sectionID, rowID) => {
-                                return (
-                                    <TouchableOpacity onPress={this.check.bind(this, rowID)}>
-                                        <View style={styles.listItem}>
-                                            <Text style={styles.listItemName}>{rowData.station_name}</Text>
-                                            <View style={styles.content}>
-                                                <View style={styles.textAll}>
-                                                    <Text style={styles.listItemValue}>{rowData.data_value}</Text>
-                                                    <Image source={rowData.check ? require('../icons/check_pre.png') : require('../icons/check_nor.png')} style={styles.checkImage}></Image>
-                                                </View>
-                                                <Text style={styles.time}>{rowData.data_time}</Text>
-                                            </View>
-                                        </View>
-                                    </TouchableOpacity>
-                                )
-                            }}
-                        />
+                        )
+                    }}
+                />
             </View>
         )
     }
@@ -121,8 +159,8 @@ export default class Gateway extends React.Component {
 // 样式
 const styles = StyleSheet.create({
     all: {
-        minHeight:150,
-        maxHeight:400,
+        minHeight: 150,
+        maxHeight: 400,
         backgroundColor: "#ffffff",
         // marginTop: 20,
     },
